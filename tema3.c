8815445c6de49e29ffdc802ce9b75fba74540937
bogdan <bogdanmica36@gmail.com>
2020-05-10 09:25:56 +0300

    citirea si indentarea

--- tema3.c
+++ tema3.c
@@ -35,10 +35,105 @@ void nameVal(char **name,char **val,char *eticheta)
  strcpy(*val,p);
  free(eticheta);
 }
+void adaugaID(char **id)
+{	
+	char *copie = calloc(20,sizeof(char));
+	strcpy(copie,*id);
+	char *p = strtok(copie,".");
+	char ultim;
+	while(p)
+	{
+		ultim = p[0];
+		p = strtok(NULL,".");
+	}
+	ultim++;
+	(*id)[strlen(*id)-1] = ultim;
+	free(copie);
+}
+//parcurgere in latime arbore
+void bfsID(TArb root)
+{
+	void *queue = InitQ(sizeof(TNodArb));
+	IntrQ(queue,(void*)root);
+	
+	while(!EMPTYQ(queue))
+	{
+		TArb scos = calloc(1,sizeof(TNodArb));// TRE ELIBERAT
+		ExtrQ(queue,scos);
+		TArb p;
+		char id = '1';
+		for(p = scos->firstChild; p != NULL ; p = p->nextSibling){
+			if(scos->info->id == NULL){
+				if(!p->info->id)
+					p->info->id = calloc(30,sizeof(char));
+				p->info->id[0] = id;
+				id++;
+			}
+			else
+			{	if(!p->info->id)
+				p->info->id = calloc(30,sizeof(char));
+				strcpy(p->info->id,scos->info->id);
+				strcat(p->info->id,".");
+				p->info->id[strlen(p->info->id)] = id++;
+			}
+			IntrQ(queue,(void*)p);
+		}
+	}
+}
+int doarSpatii(char *content)
+{
+	for(int i = 0; i < strlen(content); i++)
+		if(content[i] != ' ')
+			return 0;
+	return 1;
+}
+//afisare cu indentare
+void RSD(TArb root,int *taburi)
+{
+	if(!root)
+		return;
+	else
+	{	for(int i = 0; i < *taburi; i++)
+			printf("\t");
+		if(root->info)
+		printf("<%s",root->info->type);
+		if(root->info->style){
+			printf(" style=\"");
+		TAttr st;
+		for( st = root->info->style; st->next != NULL; st = st->next)
+			printf("%s: %s; ",st->name,st->value);
+		printf("%s: %s;\" ",st->name,st->value);//la ultimul atribute nu exista spatiu intre ; si "  de la sfarsit
+		}
+		if(root->info->otherAttributes)
+		{	printf(" ");
+			TAttr attr;
+			for(attr = root->info->otherAttributes; attr->next != NULL; attr = attr->next)
+				printf("%s=\"%s\" ",attr->name,attr->value);
+			printf("%s=\"%s\"",attr->name,attr->value);
+		}
+		printf(">\n");
+		//am afisat atributele la stil si celelalte atribute 
+		//acum urmeaza continut daca e dif de null
+		if(root->info->contents && strlen(root->info->contents) > 0){
+			for(int i = 0; i < (*taburi); i++)
+				printf("\t");
+			printf("\t");
+			printf("%s\n",root->info->contents);
+		}
+		(*taburi)++;
+		for(TArb p = root->firstChild; p != NULL ;p = p->nextSibling)
+			RSD(p,taburi);
+		(*taburi)--;
+		for(int i = 0; i < *taburi; i++)
+			printf("\t");
+		printf("</%s>\n",root->info->type);
+
+	}
+}
 
 int main(int argc, char **argv)
 {
-	FILE *in = fopen("ex.html","r");
+	FILE *in = fopen("simple.html","r");
 	if(!in)
 		return 1;
 	char c = ' ';
@@ -47,10 +142,9 @@ int main(int argc, char **argv)
 		return 1;
 	b = a;
 	TParseState currentState = 1,nextState;
-	char *tmp = calloc(30,sizeof(char)),*value = calloc(100,sizeof(char));
+	char *tmp = calloc(100,sizeof(char)),*value = calloc(100,sizeof(char));
 	void *st = NULL;
 	int nr = 0;
-	
 	while((c = fgetc(in)) != EOF)
 		{
 		nextState = Interpret(currentState,c);
@@ -65,8 +159,10 @@ int main(int argc, char **argv)
 			if(!scos->info)	
 			scos->info = calloc(1,sizeof(TNodInfo));
 			if(!scos->info->contents)
-				scos->info->contents = calloc(30,sizeof(char));
+				scos->info->contents = calloc(100,sizeof(char));
+			if((int)c != 13) // lua contents fara niciun carac
 			scos->info->contents[strlen(scos->info->contents)] = c;
+			
 			PushS(st,(void*)scos);
 			}
 		}
@@ -75,10 +171,17 @@ int main(int argc, char **argv)
 			if(!new->info)
 				new->info = calloc(1,sizeof(TNodInfo));
 			if(!new->info->type)
-				new->info->type = calloc(30,sizeof(char));
+				new->info->type = calloc(100,sizeof(char));
 			new->info->type[strlen(new->info->type)] = c;
+			if(!new->info->contents)
+				new->info->contents = calloc(100,sizeof(char));
 			if(!st)
+				{new->info->id = NULL;
 				st = InitS(sizeof(TNodArb));
+				}
+			else 
+				new->info->id = calloc(20,sizeof(char));
+
 			PushS(st,(void*)new);
 		}
 		else if(currentState == 3 && nextState == 3)
@@ -90,7 +193,7 @@ int main(int argc, char **argv)
 			if(!scos->info)	
 			scos->info = calloc(1,sizeof(TNodInfo));
 			if(!scos->info->type)
-				scos->info->type = calloc(30,sizeof(char));
+				scos->info->type = calloc(100,sizeof(char));
 			scos->info->type[strlen(scos->info->type)] = c;
 			PushS(st,(void*)scos);
 			}
@@ -103,21 +206,20 @@ int main(int argc, char **argv)
 				PopS(st,scos);
 				TArb tata = calloc(1,sizeof(TNodArb));
 				int rez = PopS(st,tata);
-				
-				if(!rez){
+				if(!rez)
 					a = scos;
-					//printf("%s\n",a->info->type);
-				}
-				else{ 
-				if(!tata->firstChild)
+				else{
+				if(!tata->firstChild){
 					tata->firstChild = scos;
+				}
 				else
-					{	TArb p = tata->firstChild;
-						while(p->nextSibling != NULL)
-							p = p->nextSibling;
-						p->nextSibling = scos;
+					{	
+					TArb p = tata->firstChild;
+					while(p->nextSibling != NULL)
+						p = p->nextSibling;
+					p->nextSibling = scos;
 					}
-				PushS(st,(void*)tata);
+					PushS(st,(void*)tata);
 				}
 			}
 		}
@@ -129,7 +231,6 @@ int main(int argc, char **argv)
 			tmp[strlen(tmp)] = c;
 		else if(currentState == 8 && nextState == 5){
 			//adauga valoarea atributului	
-			//printf("%s=\"%s\"\n",tmp,value);
 			TArb scos = calloc(1,sizeof(TNodArb));
 			int rez = PopS(st,scos);
 			if(rez)
@@ -167,7 +268,8 @@ int main(int argc, char **argv)
 					nameVal(&nume,&val,sir[i]);
 					trim(&val);
 					if(!scos->info->style)
-						{scos->info->style = calloc(1,sizeof(TNodAttr));
+						{
+						scos->info->style = calloc(1,sizeof(TNodAttr));
 						scos->info->style->name = nume;
 						scos->info->style->value = val;
 						}
@@ -191,37 +293,38 @@ int main(int argc, char **argv)
 		else if(currentState == 9 && nextState == 1){
 			//self closing
 			TArb scos = calloc(1,sizeof(TNodArb));
-			int rez = PopS(st,scos);
+			PopS(st,scos);
+			TArb tata = calloc(1,sizeof(TNodArb));
+			int rez = PopS(st,tata);
 			if(rez)
 			{
 			if(!scos->info)	
 				scos->info = calloc(1,sizeof(TNodInfo));
+			if(!scos->info->type)
+				scos->info->type = calloc(100,sizeof(char));
+			if(!scos->info->contents)
+				scos->info->contents = calloc(100,sizeof(char));
+			if(!scos->info->id)
+				scos->info->id = calloc(100,sizeof(char));
 			scos->info->isSelfClosing = 1;
-			PushS(st,(void*)scos);
+			if(!tata->firstChild)
+				tata->firstChild = scos;
+			else
+				{	
+				TArb p = tata->firstChild;
+				while(p->nextSibling != NULL)
+					p = p->nextSibling;
+				p->nextSibling = scos;
+				}
 			}
+			else  a = scos;
+			PushS(st,(void*)scos);
 		}
 		currentState = nextState;
 	}
-	
-	for(b = a;b != NULL;b = b->nextSibling){
-		if(b->info->type)
-			printf("%s\n",b->info->type);
-		else
-			printf("tata\n");
-		for(TArb p = b->firstChild; p != NULL ; p = p->nextSibling){
-			if(p->info)
-				{if(p->info->type)
-					printf("%s ",p->info->type);
-				}
-			else printf("nu ");
-			if(p->info->style){
-			TAttr px = p->info->style;
-			for(;px != NULL;px = px->next)
-			printf("%s %s ",px->name,px->value);
-			}	
-	
-		}
-	}
-	
+	//adaugare id prin parcurgere in latime
+	bfsID(a);
+	int taburi = 0;
+	RSD(a,&taburi);//indentare
 	return 0;
 }
